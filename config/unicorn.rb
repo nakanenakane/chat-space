e1dc5c61aecb3578988feabffaf9d33d942c0af2
nakanenakane <nandakakanashe@gmail.com>
2020-08-22 12:01:00 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,6 +10,7 @@ pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
+listen 3000
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
 
